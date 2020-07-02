45dd3fba57dcacaa2583cc71035f5f3386185eaf
harryjjacobs <harryjjacobs@googlemail.com>
2020-07-02 17:45:17 +0100

    Added orthogonal routing and fixed issues with force layout.

---
+++ src/orthogonal-routing.js
@@ -0,0 +1,411 @@
+import BinaryHeap from "./binary-heap";
+/**
+ * Orthogonal connector routing between points.
+ * Loosely based on:
+ * Wybrow, Michael, Kim Marriott, and Peter J. Stuckey.
+ * "Orthogonal connector routing."
+ * International Symposium on Graph Drawing.
+ * Springer, Berlin, Heidelberg, 2009.
+ */
+export default function OrthogonalRouter() {
+  this._obstacleCorners = [];
+  this._connectorPoints = [];
+  this._h = []; // interesting horizontal segments
+  this._v = []; // interesting vertical segments
+  this._poi = []; // points of interest used 
+  this._ovg = []; // orthogonal visibility graph
+  this._dirtyAStarNodes = []; // nodes that have been used for A* and need resetting
+}
+
+/**
+ * 
+ * @param {Point[][]} obstacleCorners array containing each set of four corners for each obstacle
+ */
+OrthogonalRouter.prototype.setObstacles = function(obstacleCorners) {
+  this._obstacleCorners = obstacleCorners;
+}
+
+/**
+ * Points from and to which orthogonal paths can be created
+ * @param {Point[]} connectorPoints
+ */
+OrthogonalRouter.prototype.setConnectorPoints = function(connectorPoints) {
+  this._connectorPoints = connectorPoints;
+}
+
+/**
+ * Uses the connector points and obstacles added in order to generate a routing
+ * graph which is used by findRoute(). Must be called prior to route finding.
+ * @param {Object} area Rect within which the orthogonal paths will be confined
+ * @param {number} area.x
+ * @param {number} area.y
+ * @param {number} area.w
+ * @param {number} area.h
+ */
+OrthogonalRouter.prototype.generateOrthogonalGraph = function(area) {
+  let ovg = [];
+  // generate poi and sets of interesting segments h and v
+  const result = _findInterestingSegments(this._connectorPoints, this._obstacleCorners, area);
+  const h = result.h;
+  const v = result.v;
+  const poi = result.poi;
+  this._h = h;
+  this._v = v;
+  // intersect pairs of segments from H and V
+  ovg = _intersectInterestingSegments(h, v);
+  this._poi = poi;
+  this._ovg = ovg;
+  console.log(this._ovg);
+}
+
+/**
+ * A-star path-finding for orthogonal connectors. Finds a route between
+ * two points in the orthogonal visibility graph. OVG must be computed
+ * before calling this.
+ * Inspiration taken from https://briangrinstead.com/blog/astar-search-algorithm-in-javascript-updated/
+ * and https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode
+ * @param {Point} start ovg connector position
+ * @param {Point} end ovg connector position
+ */
+OrthogonalRouter.prototype.findRoute = function(start, end) {
+  // reset nodes used in previous route
+  while (this._dirtyAStarNodes.length) {
+    let n = this._dirtyAStarNodes.pop();
+    n.visited = false;
+    n.closed = false;
+    n.parent = null;
+    n.h = 0;
+    n.g = 0;
+    n.f = 0;
+  }
+  // if start and end not in ovg - find their equivalent positions
+  if (!start.neighbours) {
+    start = this._connectorPoints.find(p => _arePointsEqual(start, p));
+  }
+  if (!end.neighbours) {
+    end = this._connectorPoints.find(p => _arePointsEqual(end, p));
+  }
+  if (!(end || {}).neighbours || !(start || {}).neighbours) {
+    throw "Invalid start and end positions. No connector points available at these positions. Has the OVG been generated?";
+  }
+  // set of open nodes is a binary heap, sorted by (minimum) f value
+  const openNodes = new BinaryHeap(n => n.f);
+  // reset first node and add to open set
+  start.g = 0;
+  start.h = _manhattanHeuristic(start, end);
+  openNodes.push(start);
+  // mark node to be cleaned before future routing
+  this._dirtyAStarNodes.push(start);
+  // iterate through open set of nodes, sorted in order of lowest f score
+  while (openNodes.size() > 0) {
+    // get node with lowest f score
+    const currentNode = openNodes.pop();
+    if (currentNode === end || _arePointsEqual(currentNode, end)) {
+      // we have reached the end
+      return _reconstructAStarPath(currentNode);
+    }
+    // close the current node now that we have chosen it
+    // (equivalent to adding it to a closed set)
+    currentNode.closed = true;
+    // loop over neighbours
+    for (const [_, neighbour] of Object.entries(currentNode.neighbours)) {
+      if (neighbour.closed) continue;
+      // keep track of whether node has been visited
+      // before so that score can be updated in binary
+      // heap
+      const previouslyVisited = neighbour.visited;
+      // g is distance from start node to neighbour, through current
+      // pointDistanceSquared is the edge weight between current and this neighbour.
+      const tentativeG = currentNode.g + _pointDistanceSquared(currentNode, neighbour);
+      if (!previouslyVisited || tentativeG < neighbour.g) {
+        neighbour.g = tentativeG;
+        // h is the heuristic that estimates the cost of the
+        // cheapest path from neighbour node to the end
+        neighbour.h = _manhattanHeuristic(neighbour, end);
+        // f is the total cost that we want to minimise
+        neighbour.f = neighbour.g + neighbour.h;
+        // set parent so that the path can be reconstructed later
+        neighbour.parent = currentNode;
+        neighbour.visited = true;
+        // mark node to be cleaned before future routing
+        this._dirtyAStarNodes.push(neighbour);
+      }
+      if (previouslyVisited) {
+        // the binary heap needs updating now that the f score
+        // is lower - bubble up the newly changed neighbour
+        // element to the top
+        openNodes.bubbleUpElement(neighbour);
+      } else {
+        // add to binary heap (and automatically sort it)
+        openNodes.push(neighbour);
+      }
+    }
+  }
+  // did not reach the end point
+  return false;
+}
+
+function _reconstructAStarPath(endNode) {
+  let path = [endNode];
+  while (endNode.parent) {
+    endNode = endNode.parent;
+    path.push(endNode);
+  }
+  return path.reverse();
+}
+
+function _manhattanHeuristic(a, b) {
+  var distanceX = Math.abs(b.x - a.x);
+  var distanceY = Math.abs(b.y - a.y);
+  return distanceX + distanceY;
+}
+
+/**
+ * Squared euclidean distance between two points
+ * @param {Point} a 
+ * @param {Point} b 
+ */
+function _pointDistanceSquared(a, b) {
+  return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+}
+
+/**
+ * Assembles point of interset (poi) from the connector points and obstacles' corners.
+ * Travels north, south, east, west from a point until a table is intersected or the edge of the layout is hit
+ * Returns the resulting h and v segments
+ * @param {Point[]} connectorPoints The connector points (and any additional points of interest - excluding obstacle corners.)
+ * @param {Point[][]} obstaclesCorners The obstacle's corners
+ * @param {Object} area Rect in which to find segments. Area of the layout.
+ * @param {number} area.x
+ * @param {number} area.y
+ * @param {number} area.w
+ * @param {number} area.h
+ * @returns {Object} object containing h and v segments
+ */
+function _findInterestingSegments(connectorPoints, obstaclesCorners, area) {
+  let poi = [...connectorPoints];
+  let h = [];
+  let v = [];
+  // find segments between table corners first and then add to poi
+  obstaclesCorners.forEach(corners => {
+    const tl = corners[0];
+    const tr = corners[1];
+    const bl = corners[2];
+    const br = corners[3];
+    // neighbours clockwise from north
+    tl.neighbours = { east: tr, south: bl };
+    tr.neighbours = { west: tl, south: br };
+    bl.neighbours = { north: tl, east: br };
+    br.neighbours = { north: tr, west: bl };
+    // Add the obstacle corners as points of interest
+    poi.push(tl, tr, bl, br);
+    // Create horizontal and vertical segments for table edges
+    // as they will be skipped during the process below due
+    // to the manual neighbour assignment above.
+    v.push({ a: tl, b: bl }, { a: tr, b: br });
+    h.push({ a: tl, b: tr }, { a: bl, b: br });
+  });
+  // find horizontal and vertical segments for each point of interest
+  poi.forEach(point => {
+    let north = area.y;
+    let south = area.y + area.h;
+    let west = area.x;
+    let east = area.x + area.w;
+    if (!point.neighbours) point.neighbours = {};
+    obstaclesCorners.forEach(obstCorners => {
+      // North
+      if (!point.neighbours.north && !point.horizontalOnly) {
+        // point does not yet have a northerly neighbour, so find one
+        if (point.x >= obstCorners[0].x && point.x <= obstCorners[1].x && point.y > obstCorners[2].y) {
+          // point in x-range of table and bottom table edge is above point
+          if (obstCorners[2].y > north) {
+            // smaller distance to table edge found
+            north = obstCorners[2].y;
+          }
+        }
+      }
+      // South
+      if (!point.neighbours.south && !point.horizontalOnly) {
+        // point does not yet have a southerly neighbour, so find one
+        if (point.x >= obstCorners[0].x && point.x <= obstCorners[1].x && point.y < obstCorners[0].y) {
+          // point in x-range of table and top table edge is below point
+          if (obstCorners[0].y < south) {
+            // smaller distance to table edge found
+            south = obstCorners[0].y;
+          }
+        }
+      }
+      // East
+      if (!point.neighbours.east && !point.verticalOnly) {
+        // point does not yet have a easterly neighbour, so find one
+        if (point.y >= obstCorners[0].y && point.y <= obstCorners[2].y && point.x < obstCorners[0].x) {
+          // point in y-range of table and top table edge is below point
+          if (obstCorners[0].x < east) {
+            // smaller distance to table edge found
+            east = obstCorners[0].x;
+          }
+        }
+      }
+      // West
+      if (!point.neighbours.west && !point.verticalOnly) {
+        // point does not yet have a easterly neighbour, so find one
+        if (point.y >= obstCorners[0].y && point.y <= obstCorners[2].y && point.x > obstCorners[1].x) {
+          // point in y-range of table and top table edge is below point
+          if (obstCorners[1].x > west) {
+            // smaller distance to table edge found
+            west = obstCorners[1].x;
+          }
+        }
+      }
+    });
+    if (!point.neighbours.north && !point.horizontalOnly) {
+      let northNeighbour = { x: point.x, y: north, neighbours: { south: point } };
+      point.neighbours.north = northNeighbour;
+      v.push({ a: point, b: northNeighbour });
+    }
+    if (!point.neighbours.south && !point.horizontalOnly) {
+      let southNeighbour = { x: point.x, y: south, neighbours: { north: point } };
+      point.neighbours.south = southNeighbour;
+      v.push({ a: point, b: southNeighbour });
+    }
+    if (!point.neighbours.east && !point.verticalOnly) {
+      let eastNeighbour = { x: east, y: point.y, neighbours: { west: point } };
+      point.neighbours.east = eastNeighbour;
+      h.push({ a: point, b: eastNeighbour });
+    }
+    if (!point.neighbours.west && !point.verticalOnly) {
+      let westNeighbour = { x: west, y: point.y, neighbours: { east: point } };
+      point.neighbours.west = westNeighbour;
+      h.push({ a: point, b: westNeighbour });
+    }
+  });
+  return { h: h, v: v, poi: poi };
+}
+
+function _intersectInterestingSegments(h, v) {
+  let ovg = [];
+  for (let hIndex = 0; hIndex < h.length; hIndex++) {
+    const hSeg = h[hIndex];
+    for (let vIndex = 0; vIndex < v.length; vIndex++) {
+      const vSeg = v[vIndex];
+      let intersection = _intersectLineSegments(hSeg, vSeg);
+      // check if intersection is at end of h line segment - if so
+      // use that point instead of creating a new one
+      if (_arePointsEqual(intersection, hSeg.a)) intersection = hSeg.a;
+      if (_arePointsEqual(intersection, hSeg.b)) intersection = hSeg.b;
+      if (intersection) {
+        // skip intersections at table corners
+        // this assumes that the points in these two segments
+        // are already neighbours.
+        if (_areSegmentsConnected(hSeg, vSeg)) continue;
+        intersection.neighbours = {};
+        if (hSeg.a.x < intersection.x) {
+          hSeg.a.neighbours.east = intersection;
+          intersection.neighbours.west = hSeg.a;
+          hSeg.b.neighbours.west = intersection;
+          intersection.neighbours.east = hSeg.b;
+        } else {
+          hSeg.a.neighbours.west = intersection;
+          intersection.neighbours.east = hSeg.a;
+          hSeg.b.neighbours.east = intersection;
+          intersection.neighbours.west = hSeg.b;
+        }
+        if (vSeg.a.y > intersection.y) {
+          // split vertical line segment into two segments
+          const topSeg = { a: intersection, b: vSeg.b };
+          const bottomSeg = { a: vSeg.a, b: intersection };
+          // remove original segment from vertical list
+          // and replace with the two new ones
+          v.splice(vIndex, 1, topSeg, bottomSeg);
+          vIndex++; // skip past the newly inserted items
+          vSeg.a.neighbours.north = intersection;
+          intersection.neighbours.south = vSeg.a;
+          vSeg.b.neighbours.south = intersection;
+          intersection.neighbours.north = vSeg.b;
+        } else {
+          // split vertical line segment into two segments
+          const topSeg = { a: vSeg.a, b: intersection };
+          const bottomSeg = { a: intersection, b: vSeg.b };
+          // remove original segment from vertical list
+          // and replace with the two new ones
+          v.splice(vIndex, 1, topSeg, bottomSeg);
+          vIndex++; // skip past the newly inserted items
+          vSeg.a.neighbours.south = intersection;
+          intersection.neighbours.north = vSeg.a;
+          vSeg.b.neighbours.north = intersection;
+          intersection.neighbours.south = vSeg.b;
+        }
+        ovg.push(intersection);
+      }
+    }
+  }
+  return ovg;
+}
+
+/**
+ * Based on Paul Bourke http://paulbourke.net/geometry/pointlineplane/
+ * Determine the intersection point of two line segments.
+ * Return FALSE if the lines don't intersect.
+ * @param {Object} seg1 Line segment 1
+ * @param {Point} seg1.a Starting point of segment 1
+ * @param {Point} seg1.b Finishing point of segment 1
+ * @param {Object} seg2 Line segment 1
+ * @param {Point} seg2.a Starting point of segment 2
+ * @param {Point} seg2.b Finishing point of segment 2
+ */
+function _intersectLineSegments(seg1, seg2) {
+  // Check none of the lines are of length 0
+  if ((seg1.a.x === seg1.b.x && seg1.a.y === seg1.b.y) || (seg2.a.x === seg2.b.x && seg2.a.y === seg2.b.y)) {
+    return false;
+  }
+
+  let denominator = ((seg2.b.y - seg2.a.y) * (seg1.b.x - seg1.a.x) - (seg2.b.x - seg2.a.x) * (seg1.b.y - seg1.a.y));
+
+  // Lines are parallel
+  if (denominator === 0) {
+    return false;
+  }
+
+  let ua = ((seg2.b.x - seg2.a.x) * (seg1.a.y - seg2.a.y) - (seg2.b.y - seg2.a.y) * (seg1.a.x - seg2.a.x)) / denominator;
+  let ub = ((seg1.b.x - seg1.a.x) * (seg1.a.y - seg2.a.y) - (seg1.b.y - seg1.a.y) * (seg1.a.x - seg2.a.x)) / denominator;
+
+  // Is the intersection along the segments?
+  if (ua < 0 || ua > 1 || ub < 0 || ub > 1) {
+    return false;
+  }
+
+  // Return the x and y coordinates of the intersection
+  let x = seg1.a.x + ua * (seg1.b.x - seg1.a.x);
+  let y = seg1.a.y + ua * (seg1.b.y - seg1.a.y);
+
+  return { x, y };
+}
+
+/**
+ * Determines whether two segments have a shared endpoint
+ * @param {Object} seg1 segment 1
+ * @param {Object} seg2 segment 2
+ */
+function _areSegmentsConnected(seg1, seg2) {
+  return _arePointsEqual(seg1.a, seg2.a) ||
+    _arePointsEqual(seg1.a, seg2.b) ||
+    _arePointsEqual(seg1.b, seg2.a) ||
+    _arePointsEqual(seg1.b, seg2.b);
+}
+
+/**
+ * 
+ * @param {Point} p1 
+ * @param {Point} p2 
+ */
+function _arePointsEqual(p1, p2) {
+  return (Math.abs(p1.x - p2.x) < Number.EPSILON) && (Math.abs(p1.y - p2.y) < Number.EPSILON);
+}
+
+/**
+ * The point type
+ *
+ * @typedef {Object} Point
+ * @param {number} x
+ * @param {number} y
+ */
--- src/table-layout.js
+++ src/table-layout.js
@@ -1,105 +1,271 @@
-import { default as rectCollide } from "./collide-rect";
+import "d3-hierarchy";
+import "d3-force";
+import { default as bboxCollide } from "./bbox-collide";
+import OrthogonalRouter from "./orthogonal-routing";
 
 export default function() {
-  var width = 1,
-      height = 1,
-      nodeWidth = 1,
-      nodeHeight = 1,
-      tableSeparation = 10;
-  // Use top-most nodes in force directed graph
-  // Then populate them with their children
-  // Then route links between child nodes
+  var width = 50,
+    height = 50,
+    nodeWidth = 10,
+    nodeHeight = 5,
+    tableInflation = 5, // margin around table for layout and routing
+    pathShift = 3; // amount to translate path points by in x and y to avoid overlap;
 
   function tableLayout(nodes, links) {
-    const layout = {tables: nodes, nodes: {}, links: links};
-    computeFlattenedHierarchy(layout);
-    computeForceDirectedTableLayout(layout);
-    computeNodePositionsInTables(layout);
-    // TODO: compute links
-    console.log(layout);
+    const layout = { tables: {}, nodes: nodes, links: links || [] };
+    _computeTableNodeHierarchy(layout);
+    _computeForceDirectedTableLayout(layout);
+    _computeNodePositionsInTables(layout);
+    _computeNodeLinks(layout);
+    _computeLinkPoints(layout);
     return layout;
   }
 
-  tableLayout.size = function(_) {
-    return arguments.length ? (width = _[0], height = _[1], tableLayout) : [width, height]
+  /**
+   * Sets the size within which to layout the tables
+   * @param {Number[]} size [width, height]
+   */
+  tableLayout.size = function(size) {
+    return arguments.length ? (width = size[0], height = size[1], tableLayout) : [width, height]
   }
 
-  tableLayout.nodeSize = function(_) {
-    return arguments.length ? (nodeWidth = _[0], nodeHeight = _[1], tableLayout) : [nodeWidth, nodeHeight]
+  /**
+   * Sets the size of the nodes contained in each table
+   * @param {Number[]} nodeSize [x, y]
+   */
+  tableLayout.nodeSize = function(nodeSize) {
+    return arguments.length ? (nodeWidth = nodeSize[0], nodeHeight = nodeSize[1], tableLayout) : [nodeWidth, nodeHeight]
   }
 
-  tableLayout.tableSeparation = function(_) {
-    return arguments.length ? (tableSeparation = _, tableLayout) : tableSeparation
+  /**
+   * Sets the inflation margin around each table - used for layout and routing purposes.
+   * Table inflation determines table separation when laying out and also connector edge
+   * positioning when creating links.
+   * @param {Number} inflation 
+   */
+  tableLayout.tableInflation = function(inflation) {
+    return arguments.length ? (tableInflation = inflation, tableLayout) : tableInflation
   }
 
-  function computeFlattenedHierarchy(layout) {
+  function _computeTableNodeHierarchy(layout) {
     // A root node must be created as an ancestor 
     // to the provided nodes in order to use hierarchy()
     var root = {
       id: "root",
-      children: layout.tables
+      children: layout.nodes
     };
     // Organise into a hierarchical structure
     root = d3.hierarchy(root)
-    // Remove root node and shift its children up a
-    // level so that tables are top-level and nodes
-    // are below tables
+      // Remove root node and shift its children up a
+      // level so that tables are top-level and nodes
+      // are below tables
     var tables = root.children;
     tables.forEach(table => {
-      table.parent = null;
+      table.parent = null; // tables are top-level
+      delete table.height; // unnecessary and would cause confusion with table rect height
       table.descendants().forEach(n => n.depth -= 1);
     });
     layout.tables = tables;
     layout.nodes = layout.tables.flatMap(t => t.children);
   }
 
-  function computeForceDirectedTableLayout(layout) {
+  function _computeForceDirectedTableLayout(layout) {
     // Apply force simulation to each of the tables in order
     // to distribute them evenly. Perform collision handling
     // using the bounding box of the table rather than a 
     // radius, for better positioning
+    const collide = bboxCollide(d => {
+      console.log(d);
+      const width = _computeInflatedTableWidth(d);
+      const height = _computeInflatedTableHeight(d);
+      return [
+        [-width / 2 - 5, -height / 2 - 5],
+        [width / 2 + 5, height / 2 + 5]
+      ];
+    });
     var sim = d3.forceSimulation(layout.tables)
-    // TODO: use forceLink to move tables with linked nodes closer together
-      .force('link', d3.forceLink().id(d => d.id))
-      //.force("collide", d3.forceCollide().radius(function(d) { return Math.max(computeTableWidth(d), computeTableHeight(d)) / 2.0 + tableSeparation; }))
-      .force("collide", rectCollide().size(d => [computeTableWidth(d) + tableSeparation, computeTableHeight(d) + tableSeparation]))
-      .force("charge", d3.forceManyBody())
+      // TODO: use forceLink to move tables with linked nodes closer together
+      //.force('link', d3.forceLink().id(d => d.id))
+      //.force("collide", d3.forceCollide().radius(function(d) { return Math.max(computeTableWidth(d), computeTableHeight(d)) / 2.0 + tablePadding; }))
+      //.force("collide", rectCollide().size(d => [_computeInflatedTableWidth(d), _computeInflatedTableWidth(d)]))
+      .force("collide", collide)
+      .force("charge", d3.forceManyBody().strength(1000))
       .force("center", d3.forceCenter(width / 2.0, height / 2.0))
       .stop();
     // Run simulation for x ticks in order to position the tables
-    sim.tick(300);
+    for (var i = 0; i < 300; ++i) {
+      // constrain each table within the layout area
+      layout.tables.forEach(table => {
+        const tableWidthExtent = _computeInflatedTableWidth(table) / 2 + 10;
+        const tableHeightExtent = _computeInflatedTableHeight(table) / 2 + 10;
+        table.x = Math.min(Math.max(table.x, tableWidthExtent), width - tableWidthExtent);
+        table.y = Math.min(Math.max(table.y, tableHeightExtent), height - tableHeightExtent);
+      });
+      sim.tick();
+    }
+    //sim.tick(300);
     // Adjust table positions as position from force simulation
     // is at the center of the object
     layout.tables.forEach(t => {
-      t.x -= computeTableWidth(t) / 2.0;
-      t.y -= computeTableHeight(t) / 2.0;
+      var width = _computeTableWidth(t);
+      var height = _computeTableHeight(t);
+      t.x -= width / 2.0;
+      t.y -= height / 2.0;
+      t.w = width;
+      t.h = height;
+      t.inflatedRect = _computeInflatedTableRect(t);
     });
   }
 
-  function computeNodePositionsInTables(layout) {
+  function _computeNodePositionsInTables(layout) {
     // Position each table's nodes
     layout.tables.forEach(table => {
-      var n = ('children' in table) ? table.children.length : 0;
-      var y = table.y;
-      var x = table.x;
+      let n = ('children' in table) ? table.children.length : 0;
+      let y = table.y;
+      let x = table.x;
       for (let i = 0; i < n; i++) {
         const child = table.children[i];
         child.x = x;
         child.y = y;
         y += nodeHeight;
       }
-      table.w = nodeWidth;
-      table.h = nodeHeight * n;
     });
   }
 
-  function computeTableHeight(table) {
-    return ('children' in table) ? (nodeHeight * table.children.length) : 0;
+  function _computeNodeLinks(layout) {
+    // each node will store incoming (target) and outgoing (source) links
+    layout.nodes.forEach((node, i) => {
+      node.index = i;
+      node.sourceLinks = [];
+      node.targetLinks = [];
+    });
+    // convert link source and target from indices to node references
+    layout.links.forEach((link, i) => {
+      link.index = i;
+      console.log(link);
+      if (typeof link.source !== "object") link.source = layout.nodes.find(n => n.index === link.source);
+      if (typeof link.target !== "object") link.target = layout.nodes.find(n => n.index === link.target);
+      link.source.sourceLinks.push(link);
+      link.target.targetLinks.push(link);
+    });
   }
 
-  function computeTableWidth() {
+  function _computeLinkPoints(layout) {
+    const router = new OrthogonalRouter(width, height);
+    const obstacleCorners = layout.tables.map(t => t.inflatedRect.corners);
+    const connectorPoints = layout.nodes.map(n => _computeConnectorPoint(n));
+
+    router.setObstacles(obstacleCorners);
+    router.setConnectorPoints(connectorPoints);
+
+    let t0 = performance.now();
+    // compute orthogonal visibility graph
+    router.generateOrthogonalGraph({ x: 0, y: 0, w: width, h: height });
+    let t1 = performance.now();
+    console.log(`Generating OVG took ${(t1 - t0).toFixed(2)}ms`);
+
+    // route a path for all of the (visible) links
+    // and shift points in link by a small amount
+    // to avoid overlap
+    let pathShiftAmount = 0;
+    layout.links.forEach(link => {
+      t0 = performance.now();
+      // find route using A* path finder
+      // copy points to new array of 
+      // clean points
+      link.points = router.findRoute(
+        _computeConnectorPoint(link.source),
+        _computeConnectorPoint(link.target)
+      ).map(p => ({ x: p.x, y: p.y }));
+      t1 = performance.now();
+      console.log(`A* route finder took ${(t1 - t0).toFixed(2)}ms for link`, link);
+      _performPathShift(link.points, { x: pathShiftAmount, y: pathShiftAmount });
+      pathShiftAmount += pathShift;
+    });
+
+    console.log(layout.links);
+
+    layout.poi = router._poi;
+    layout.ovg = router._ovg;
+    layout.h = router._h;
+    layout.v = router._v;
+  }
+
+  /**
+   * Translates points in an orthogonal path such that the first and last
+   * points remain the same and the others are shifted by the translation 
+   * where possible (in order for the path to remain orthogonal).
+   * @param {Point[]} path 
+   * @param {Point} translation 
+   */
+  function _performPathShift(path, translation) {
+    for (let i = 0; i < path.length; i++) {
+      const point = path[i];
+      if (i == 1) {
+        if (point.x === path[0].x) {
+          point.y += translation.y;
+        } else if (point.y === path[0].y) {
+          point.x += translation.x;
+        }
+      }
+      if (i == path.length - 2) {
+        if (point.x === path[path.length - 1].x) {
+          point.y += translation.y;
+        } else if (point.y === path[path.length - 1].y) {
+          point.x += translation.x;
+        }
+      }
+      if (i > 1 && i < path.length - 2) {
+        point.x += translation.x;
+        point.y += translation.y;
+      }
+    }
+  }
+
+  function _computeConnectorPoint(node) {
+    return { x: node.x + nodeWidth / 2, y: node.y + nodeHeight / 2, horizontalOnly: true };
+    //return { x: node.x, y: node.y + nodeHeight / 2, horizontalOnly: true };
+  }
+
+  function _computeTableHeight(table) {
+    return (('children' in table) ? (nodeHeight * table.children.length) : 0);
+  }
+
+  function _computeTableWidth() {
     return nodeWidth;
   }
 
+  function _computeInflatedTableHeight(table) {
+    return (('children' in table) ? (nodeHeight * table.children.length + tableInflation * 2) : 0);
+  }
+
+  function _computeInflatedTableWidth() {
+    return nodeWidth + tableInflation * 2;
+  }
+
+  /**
+   * Inflates the table size and provides corner points.
+   * Used for layout and orthogonal connector
+   * @param {*} table 
+   */
+  function _computeInflatedTableRect(table) {
+    const x = table.x - tableInflation;
+    const y = table.y - tableInflation;
+    const w = table.w + tableInflation * 2;
+    const h = table.h + tableInflation * 2;
+    const corners = [
+      { x: x, y: y }, // tl
+      { x: x + w, y: y }, // tr
+      { x: x, y: y + h }, // bl
+      { x: x + w, y: y + h }, // br
+    ];
+    return {
+      x: x,
+      y: y,
+      w: w,
+      h: h,
+      corners: corners
+    }
+  }
   return tableLayout;
 };
